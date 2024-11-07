e06294f3d8c422fc6d844d103ab22eed06d31948
Esouder <eric@souder.ca>
2024-11-06 19:17:13 -0800

    I guess DAQ sort of working... not elegant but good enough

--- epcomms/equipment/analog_discovery/analog_discovery_DAQ.py
+++ epcomms/equipment/analog_discovery/analog_discovery_DAQ.py
@@ -1,12 +1,15 @@
 from ctypes import *
 from . import dwfconstants
 import socket
+import time
 
 
 from epcomms.connection.transmission.transmission import Transmission
 from epcomms.equipment.daq import DAQ
 
 from . import dwf
+from .dwfconstants import *
+
 
 class AnalogDiscoveryDAQ(DAQ):
     def __init__(self, device_handle: c_int) -> None:
@@ -14,7 +17,33 @@ class AnalogDiscoveryDAQ(DAQ):
         self.handle = device_handle
 
     def measure_voltage(self, channel: int) -> float:
-        pass
+        """Perform a single, untriggered voltage measurement."""
+        num_samples = 100
+        rgdSamples = (c_double*num_samples)()
+
+        dwf.FDwfAnalogInFrequencySet(self.handle, c_double(20000000.0))
+        dwf.FDwfAnalogInBufferSizeSet(self.handle, c_int(num_samples)) 
+        dwf.FDwfAnalogInChannelEnableSet(self.handle, c_int(channel), c_int(1))
+        dwf.FDwfAnalogInChannelFilterSet(self.handle, c_int(channel), c_int(1))
+
+
+        dwf.FDwfAnalogInConfigure(self.handle, c_int(channel), c_int(1))
+
+        sts = c_byte()
+        while True:
+            dwf.FDwfAnalogInStatus(self.handle, c_int(1), byref(sts))
+            if sts.value == DwfStateDone.value :
+                break
+            time.sleep(0.1)
+            print("sleeping")
+
+        dwf.FDwfAnalogInStatusData(self.handle, c_int(channel), rgdSamples, c_int(num_samples)) # get channel 1 data
+
+        return sum(rgdSamples)/len(rgdSamples)
+
+
 
     def configure_daq_channel(self, channel: int, voltage_range: float, offset_voltage: float) -> None:
-        pass
+        """Configures a channel of an instrument as a DAQ."""
+        dwf.FDwfAnalogInChannelRangeSet(self.handle, c_int(channel), c_double(voltage_range))
+        dwf.FDwfAnalogInChannelOffsetSet(self.handle, c_int(channel), c_double(offset_voltage))
--- examples/analog_discovery.py
+++ examples/analog_discovery.py
@@ -12,14 +12,32 @@ print(devices)
 
 device = AnalogDiscovery('192.168.0.32', '210018B9D801')
 
-device.function_generator.set_mode(FunctionGeneratorMode.SINE, 0)
-device.function_generator.set_frequency(0.05, 0)
-device.function_generator.set_amplitude(1, 0)
-device.function_generator.set_offset(1,0)
-#device.function_generator.set_duty_cycle(50, 0)
-device.function_generator.start_output(0, False)
-time.sleep(30)
-device.function_generator.disable_output(0)
+# device.function_generator.set_mode(FunctionGeneratorMode.SINE, 0)
+# device.function_generator.set_frequency(0.05, 0)
+# device.function_generator.set_amplitude(1, 0)
+# device.function_generator.set_offset(1,0)
+# device.function_generator.start_output(0, False)
+# time.sleep(30)
+# device.function_generator.disable_output(0)
+
+voltage = 0
+
+device.daq.configure_daq_channel(0, 5, 0)
+
+
+for i in range(5):
+    voltage += device.daq.measure_voltage(0)
+
+voltage = voltage/5
+print(voltage)
+
+device.daq.configure_daq_channel(0, 5, -1*voltage)
+
+voltage = device.daq.measure_voltage(0)
+
+print(voltage)
+
+
 
 
 
