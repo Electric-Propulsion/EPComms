36eb69ac4c8a7d6ae1b3748f1828a39fc8c25580
Esouder <eric@souder.ca>
2025-03-26 15:23:09 -0700

    more fluke functionality

--- examples/fluke45.py
+++ examples/fluke45.py
@@ -1,5 +1,41 @@
 from epcomms.equipment.multimeter.fluke45 import Fluke45
+import time
 
 mm = Fluke45("COM4")
-for i in range(10):
-    print(mm.measure_voltage_dc(None,None))
+
+print(mm.measure_continuity())
+
+
+for _range in [None, 'AUTO', 1, 2, 3, 4, 5, 6, 7]:
+    for resolution in [None, 'S', 'M', 'F']:
+        try:
+            print(mm.measure_voltage_ac(_range, resolution))
+        except Exception as e:
+            print(f"Error measuring voltage AC with range {_range} and resolution {resolution}: {e}")
+        time.sleep(1)
+
+        try:
+            print(mm.measure_voltage_dc(_range, resolution))
+        except Exception as e:
+            print(f"Error measuring voltage DC with range {_range} and resolution {resolution}: {e}")
+        time.sleep(1)
+
+        try:
+            print(mm.measure_current_ac(_range, resolution))
+        except Exception as e:
+            print(f"Error measuring current AC with range {_range} and resolution {resolution}: {e}")
+        time.sleep(1)
+
+        try:
+            print(mm.measure_current_dc(_range, resolution))
+        except Exception as e:
+            print(f"Error measuring current DC with range {_range} and resolution {resolution}: {e}")
+        time.sleep(1)
+
+        try:
+            print(mm.measure_frequency(_range, resolution))
+        except Exception as e:
+            print(f"Error measuring frequency with range {_range} and resolution {resolution}: {e}")
+        time.sleep(1)
+
+
