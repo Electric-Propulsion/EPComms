5bcc76c17148be24e526de63193d5e464ae20da9
Esouder <eric@souder.ca>
2025-12-13 16:29:44 -0800

    update examples

--- examples/BGP.py
+++ examples/BGP.py
@@ -3,8 +3,8 @@ from epcomms.equipment.vacuumcontroller.inficon_BGP400 import InficonBGP400
 def main():
     bgp400 = InficonBGP400("COM3")
     bgp400.set_mbar()
-    bgp400.degass_on()
-    bgp400.read_loop()
+    #bgp400.degass_on()
+    print(bgp400.get_state().pressure)
 
 if __name__ == "__main__":
     main()
